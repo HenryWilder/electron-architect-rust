d093f5005d70fc47ebe79394f25c5f2c86fc8395
Henry Wilder <henrythepony@gmail.com>
2024-03-18 01:53:30 -0400

    Changed cursor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
 use raylib::prelude::*;
 
+const CURSOR_WIDTH: f32 = 2.0;
+const CURSOR_SIZE: Vector2 = Vector2 {
+    x: CURSOR_WIDTH,
+    y: CURSOR_WIDTH,
+};
+const CURSOR_EXTENT: Vector2 = Vector2 {
+    x: CURSOR_WIDTH / 2.0,
+    y: CURSOR_WIDTH / 2.0,
+};
+
 fn main() {
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Hello, World")
+        .size(1280, 720)
+        .title("Electron Architect")
         .vsync()
         .build();
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+    rl.hide_cursor();
 
+    while !rl.window_should_close() {
+        // Tick
+
+        let mouse_pos = rl.get_mouse_position();
+
+        // Draw
+
+        let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::WHITE);
+
+        {
+            let mut b = d.begin_blend_mode(BlendMode::BLEND_SUBTRACT_COLORS);
+            b.draw_rectangle_v(mouse_pos - CURSOR_EXTENT, CURSOR_SIZE, Color::WHITE);
+        }
     }
 }
